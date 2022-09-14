ef2590a981622cfe4130d12b3c2fe8334c38a723
Alex <abadesgrimes31@gmail.com>
2022-09-14 18:26:49 +0200

    Week 1

---
+++ Intro javascript/1-Intro.js
@@ -0,0 +1,41 @@
+console.log("Hello from JavaScript")
+window.onload = function init () 
+{
+    console.log("Hello World");
+    var number = 5;
+
+    // var decalres a variable, it's not necessary specify a value at the moment on declaration 
+    var z;
+    z = 10;
+    var h = 20;
+
+
+    // We can decalre different variables 
+    var a;
+    var b;
+    var c;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    
+
+    //  Then we can assign values 
+    a = 10
+    b = 20 
+    c = "I am a"
+    console.log(a)
+    console.log(b)
+    console.log(c)
+
+
+    // finally we can operate with this variables 
+    a = a + 10
+    b = b * 10
+    c = c + " Stirng"
+    console.log(a)
+    console.log(b)
+    console.log(c)
+
+    // Case sensitive; the capitalization matters
+
+}
